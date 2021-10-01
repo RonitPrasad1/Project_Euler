8889cc1c3448246548373c79452cced7d9dd8684
Ronit Prasad <55261813+RonitPrasad1@users.noreply.github.com>
2021-10-01 12:35:40 -0500

    Add files via upload

---
+++ Question 9.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+int main() 
+{
+	long long int n, a, b, c, Squares, Product = 0;
+	std::cin >> n;
+
+	for (a = 1; a <= n; ++a)
+	{
+		for (b = a + 1; b <= n; ++b)
+		{
+			Squares = (a * a) + (b * b);
+			for (c = a + 2; c <= n; ++c)
+			{
+				if (Squares == std::pow(c, 2) && (a + b + c == n))
+                {
+                    Product = a * b * c;
+                    //std::cout << a << " " << b << " " << c << '\n';
+                }
+			}
+		}
+    }
+    std::cout << Product << '\n';
+    return 0;
+}
