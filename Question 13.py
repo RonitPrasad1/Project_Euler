106d70686e59e49b9e96a5f0c13939bbadf27f42
Ronit Prasad <55261813+RonitPrasad1@users.noreply.github.com>
2021-10-02 22:57:06 -0500

    Add files via upload

---
+++ Question 13.py
@@ -0,0 +1,15 @@
+problem = 'ProblemEuler 13.txt'
+
+with open(problem, "r") as ins:
+    array = []
+
+    for line in ins:
+        array.append(line)
+
+    Store = []
+
+    for i in Store 
+        newArray.append(int(i))
+
+    Sum = sum(Store)
+    print str(Sum)[:10]
